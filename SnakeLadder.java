6b76f16087d7e0366472cea33bae3d093177ef69
sunnyok03 <sunnyok03@gmail.com>
2023-11-23 23:52:22 +0530

    UC2_get_dice_num

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,5 +4,8 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Starting with single player from position 0.");
         int currPos = 0;
+
+        int diceNum = (int)(Math.random()*6)+1; // dice number from 1 to 6
+
     }
 }
