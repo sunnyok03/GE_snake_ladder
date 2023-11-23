5b148c9d03150100b5cbc2b87abeded04e23af08
sunnyok03 <sunnyok03@gmail.com>
2023-11-23 23:49:46 +0530

    UC1_start_game_with_single_player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,5 +2,7 @@ package com.snake_ladder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
+        System.out.println("Starting with single player from position 0.");
+        int currPos = 0;
     }
 }
